5c5556abca30ffa597d3f22aa2a9d686b61f6f80
kalush666 <jonathan.kalush@gmail.com>
2025-07-27 14:03:15 +0300

    UAV entity

---
+++ Simulation/Common/Enums/UAVTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace Simulation.Common.Enums
+{
+    public enum UAVTypes
+    {
+        A1, A2, A3, A4, A5, A6
+    }
+}
---
+++ Simulation/Models/UAV.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Simulation.Common.Enums;
+
+namespace Simulation.Models
+{
+    public class UAV
+    {
+        [BsonId]
+        [BsonElement("_id"),BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; }
+
+        [BsonRepresentation(BsonType.String)]
+        public UAVTypes UAVType { get; set; }
+
+        [BsonRepresentation(BsonType.Int64)]
+        public int CurrentMissionId { get; set; }
+    }
+}
