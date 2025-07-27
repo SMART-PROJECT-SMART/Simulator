f48288264ea310ce58cc3a64a5dcb2bda398dcab
kalush666 <jonathan.kalush@gmail.com>
2025-07-27 16:13:16 +0300

    Mission Entity

---
+++ Simulation/Models/Mission/Mission.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Simulation.Models.Mission
+{
+    public class Mission
+    {
+        [BsonId]
+        [BsonElement("_id"), BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public Location Destination { get; set; }
+
+        public int WingId { get; set; }
+
+        public Mission(Location destination, int wingId)
+        {
+            Destination = destination;
+            WingId = wingId;
+        }
+    }
+}
