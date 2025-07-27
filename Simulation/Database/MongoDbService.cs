bcadbbd03e3851ca59ef05f03e944d1a5cd19e12
kalush666 <jonathan.kalush@gmail.com>
2025-07-27 13:49:30 +0300

    mongo db connection

---
+++ Simulation/Database/MongoDbService.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using Simulation.constants;
+
+namespace Simulation.Database
+{
+    public sealed class MongoDbService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase? _database;
+
+        private MongoDbService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            string connectionString = _configuration.GetConnectionString(SimulationConstants.Configuration.DB_CONNECTION);
+            MongoUrl mongoUrl = MongoUrl.Create(connectionString);
+            MongoClient mongoClient = new MongoClient(mongoUrl);
+            _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+        }
+
+        private static MongoDbService _instance;
+
+        public static MongoDbService GetInstance(IConfiguration configuration)
+        {
+            _instance ??= new MongoDbService(configuration);
+            return _instance;
+        }
+    }
+}
---
+++ Simulation/constants/SimulationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Simulation.constants
+{
+    public static class SimulationConstants
+    {
+        public static class Configuration {
+            public static string DB_CONNECTION = "DbConnection";
+        }
+    }
+}
