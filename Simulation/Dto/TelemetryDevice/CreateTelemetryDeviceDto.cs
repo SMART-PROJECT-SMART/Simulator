2a34a396e9d72923aff1670b8fa620bd1880f8bc
kalush666 <jonathan.kalush@gmail.com>
2025-12-29 11:16:47 +0200

    feat: Add DTO for creating telemetry devices

---
+++ Simulation/Dto/TelemetryDevice/CreateTelemetryDeviceDto.cs
@@ -0,0 +1,22 @@
+using Simulation.Models;
+
+namespace Simulation.Dto.TelemetryDevice
+{
+    public class CreateTelemetryDeviceDto
+    {
+        public int TailId { get; set; }
+        public IEnumerable<int> PortNumbers { get; set; }
+        public Location Location { get; set; }
+
+        public CreateTelemetryDeviceDto(
+            int tailId,
+            IEnumerable<int> portNumbers,
+            Location location
+        )
+        {
+            TailId = tailId;
+            PortNumbers = portNumbers;
+            Location = location;
+        }
+    }
+}
