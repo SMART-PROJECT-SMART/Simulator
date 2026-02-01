2909d66cf5a5df353afddfce3861b7443661781a
jonathan kalush <jonathan.kalush@gmail.com>
2026-02-01 08:46:28 +0200

    [feat] uav factory

---
+++ Simulation/Services/UAVFactory/IUAVFactory.cs
@@ -0,0 +1,11 @@
+using Simulation.Dto.DeviceManager;
+using Simulation.Models;
+using Simulation.Models.UAVs;
+
+namespace Simulation.Services.UAVFactory
+{
+    public interface IUAVFactory
+    {
+        UAV CreateUAV(DeviceManagerUAVDto uavDto, Location startLocation);
+    }
+}
---
+++ Simulation/Services/UAVFactory/UAVFactory.cs
@@ -0,0 +1,24 @@
+using Core.Common.Enums;
+using Simulation.Dto.DeviceManager;
+using Simulation.Models;
+using Simulation.Models.UAVs;
+using Simulation.Models.UAVs.ArmedUav;
+using Simulation.Models.UAVs.SurveillanceUAV;
+
+namespace Simulation.Services.UAVFactory
+{
+    public class UAVFactory : IUAVFactory
+    {
+        public UAV CreateUAV(DeviceManagerUAVDto uavDto, Location startLocation)
+        {
+            return uavDto.PlatformType switch
+            {
+                PlatformType.Hermes900 => new Hermes900(uavDto.TailId, startLocation),
+                PlatformType.HeronTP => new HeronTp(uavDto.TailId, startLocation),
+                PlatformType.Hermes450 => new Hermes450(uavDto.TailId, startLocation),
+                PlatformType.Searcher => new Searcher(uavDto.TailId, startLocation),
+                _ => throw new ArgumentException($"Unsupported platform type: {uavDto.PlatformType}", nameof(uavDto))
+            };
+        }
+    }
+}
